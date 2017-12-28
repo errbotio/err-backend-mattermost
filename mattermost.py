6eca9c984cf7c5eb5b083aeac4aa43e7929a7045
Grégory Romé <gregory.rome@gmail.com>
2017-12-28 20:34:27 +0100

    enable support of DIVERT_TO_THREAD option

--- mattermost.py
+++ mattermost.py
@@ -481,6 +481,12 @@ class MattermostBackend(ErrBot):
 			response.to = message.frm
 		else:
 			response.to = message.frm.room if isinstance(message.frm, RoomOccupant) else message.frm
+
+		if threaded:
+			response.extras['root_id'] = message.extras.get('root_id')
+			self.driver.posts.get_post(message.extras.get('root_id'))
+			response.parent = message
+
 		return response
 
 	def get_public_channels(self):
